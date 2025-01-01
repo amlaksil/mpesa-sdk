68b144fc289df472db2e20844185b987934f95b2
sil <silamlak@gmail.com>
2025-01-01 19:12:29 +0300

    Fix import and version test in TestVersion

--- tests/test_version.py
+++ tests/test_version.py
@@ -9,4 +9,4 @@ import unittest
 class TestVersion(unittest.TestCase):
     def test_version(self):
         """Test the version of mpesa_sdk."""
-        self.assertEqual(mpesa_sdk.__version__, "1.0.0")
+        self.assertEqual(mpesa.__version__, "1.0.0")
