31a6e1e16a2607cd96af1144440b151aeb998568
sil <silamlak@gmail.com>
2025-01-04 15:02:20 +0300

    Refactor logger setup to improve configuration

--- mpesa/utils/logger.py
+++ mpesa/utils/logger.py
@@ -7,7 +7,7 @@ variable. The log file size is managed with a rotating file handler, which
 limits the file size and keeps backup logs to avoid unbounded growth.
 
 Key Features:
-- Log level can be dynamically configured (default: INFO).
+- Log level can be dynamically configured (default: DEBUG).
 - Logs are saved in the 'logs' directory.
 - RotatingFileHandler ensures log file management with a
 maximum size of 10MB and keeps up to 5 backup logs.
@@ -19,18 +19,7 @@ from logging.handlers import RotatingFileHandler
 from mpesa.config import Config
 
 log_dir = os.getenv(Config.MPESA_LOG_DIR, os.path.join(os.getcwd(), "logs"))
-os.makedirs(log_dir, exist_ok=True)
 log_file_path = os.path.join(log_dir, "mpesa.log")
-
-formatter = logging.Formatter(
-    "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(formatter)
-
-file_handler = RotatingFileHandler(
-        log_file_path, maxBytes=10 * 1024 * 1024, backupCount=5)
-file_handler.setFormatter(formatter)
 
 
 def get_logger(name: str) -> logging.Logger:
@@ -44,17 +33,29 @@ def get_logger(name: str) -> logging.Logger:
     Returns:
         logging.Logger: The logger instance for the calling module.
     """
+    formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     logger = logging.getLogger(name)
-    logger.setLevel(Config.LOG_LEVEL)
 
-    logger.addHandler(file_handler)
+    if not logger.handlers:
+        logger.setLevel(getattr(logging, Config.LOG_LEVEL, logging.DEBUG))
+        os.makedirs(log_dir, exist_ok=True)
 
-    if Config.ENVIRONMENT != 'TEST':
-        logger.addHandler(stream_handler)
+        file_handler = RotatingFileHandler(
+                log_file_path, maxBytes=10 * 1024 * 1024, backupCount=5
+                )
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+        if Config.ENVIRONMENT != 'TEST':
+            stream_handler = logging.StreamHandler()
+            stream_handler.setFormatter(formatter)
+            logger.addHandler(stream_handler)
 
     return logger
 
 
-logger = get_logger(__name__)
-logger.info(
-    "Logger configured successfully. Log file path: %s", log_file_path)
+if __name__ == '__main__':
+    logger = get_logger(__name__)
+    logger.info(
+            "Logger configured successfully. Log file path: %s", log_file_path)
